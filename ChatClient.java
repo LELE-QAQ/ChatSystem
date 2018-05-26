8b596bb2fc678514544b37bad93cfc4106bbdb7c
乐乐 <973262592@qq.com>
2018-05-26 17:19:01 +0800

    0.4
    能够连续与服务器传输信息

--- ChatClient.java
+++ ChatClient.java
@@ -1,64 +1,82 @@
-import  java.awt.*;
-import  java.awt.event.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class ChatClient{
-	public static void main(String[] args) throws Exception
-	{
-		new ClientFrame().launch();
+public class ChatClient {
+	public static void main(String[] args) {
+		try {
+			new ClientFrame().launch();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
-class ClientFrame extends Frame{
-	
+class ClientFrame extends Frame {
+
 	Frame frame = new Frame("ChatClient");
 	TextField textField = new TextField(30);
-	TextArea tArea = new TextArea("",20,20);
+	TextArea tArea = new TextArea("", 20, 20);
 	Socket socket = null;
 	DataOutputStream dataOutputStream = null;
-	public void launch() throws Exception, Exception
-	{
-		setLocation(200,200);
+
+	public void launch() {
+		setLocation(200, 200);
 		setVisible(true);
-		//setResizable(false);
-		add(textField,"South");
-		add(tArea,"North");
+		// setResizable(false);
+		add(textField, "South");
+		add(tArea, "North");
 		pack();
 		textField.addActionListener(new TFListener());
 		this.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e)
-			{
+			public void windowClosing(WindowEvent e) {
+				disconnect();
 				System.exit(0);
 			}
 		});
-		
+
 		connect();
+
 	}
-	
-	public void connect() throws Exception, IOException
-	{
-		socket = new Socket("127.0.0.1", 8888);
-		System.out.println("Connect");
-		dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+	public void connect() {
+		try {
+			socket = new Socket("127.0.0.1", 8888);
+			System.out.println("Connect");
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	private class TFListener implements ActionListener{
-		public void actionPerformed(ActionEvent e)
-		{
+
+	public void disconnect() {
+		try {
+			dataOutputStream.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class TFListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
 			String string = textField.getText().trim();
-			tArea.setText(string); 
+			tArea.setText(string);
 			textField.setText("");
 			try {
 				dataOutputStream.writeUTF(string);
-				dataOutputStream.close();
-				
-				}catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			
+				dataOutputStream.flush();
+
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
 		}
 	}
 }
--- ChatServer.java
+++ ChatServer.java
@@ -1,33 +1,68 @@
 import java.io.DataInput;
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.*;
+import java.sql.Date;
 
 public class ChatServer {
 	boolean started = false;
-	//boolean bConnect = false;
-	public static void main(String[] args) throws Exception
-	{
+
+	public static void main(String[] args) {
 		new ChatServer().connect();
 	}
-	public void connect() throws Exception
-	{
-		ServerSocket serverSocket = null;
+
+	public void connect() {
 		Socket socket = null;
-		DataInputStream dataInputStream =null;
+		DataInputStream dataInputStream = null;
 		String string = null;
-		started =true;
-		while(started)
-		{
+		ServerSocket serverSocket = null;
+		try {
+
 			serverSocket = new ServerSocket(8888);
-			socket = serverSocket.accept();
-			System.out.println("A client connected");
-			dataInputStream = new DataInputStream(socket.getInputStream()); 
-			string = dataInputStream.readUTF();
-			System.out.println(string);
-			dataInputStream.close();
+
+		} catch (BindException e) {
+			System.out.println("port is using");
+			System.out.println("please close the client");
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try
+
+		{
+			started = true;
+			while (started) {
+				boolean bConnect = false;
+				socket = serverSocket.accept();
+				System.out.println("A client connected");
+				bConnect = true;
+				dataInputStream = new DataInputStream(socket.getInputStream());
+				while (bConnect) {
+					string = dataInputStream.readUTF();
+					System.out.println(string);
+				}
+				// dataInputStream.close();
+
+			}
+
+		} catch (EOFException e) {
+			System.out.println("client close");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (dataInputStream != null) {
+					dataInputStream.close();
+				}
+				if (socket != null) {
+					socket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
-}
 
+}
