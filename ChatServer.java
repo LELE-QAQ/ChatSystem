81fc6b020094cd815c59f4f2a5d904ab4a1665cc
乐乐 <973262592@qq.com>
2018-05-26 17:51:47 +0800

    0.5
    添加多个客户端与服务器传输数据

--- ChatServer.java
+++ ChatServer.java
@@ -5,17 +5,21 @@ import java.io.IOException;
 import java.net.*;
 import java.sql.Date;
 
+import com.sun.tools.classfile.Annotation.element_value;
+
 public class ChatServer {
-	boolean started = false;
 
 	public static void main(String[] args) {
-		new ChatServer().connect();
+		new Server().connect();
 	}
+}
+
+class Server {
+	boolean started = false;
 
 	public void connect() {
 		Socket socket = null;
 		DataInputStream dataInputStream = null;
-		String string = null;
 		ServerSocket serverSocket = null;
 		try {
 
@@ -36,33 +40,55 @@ public class ChatServer {
 				boolean bConnect = false;
 				socket = serverSocket.accept();
 				System.out.println("A client connected");
-				bConnect = true;
-				dataInputStream = new DataInputStream(socket.getInputStream());
-				while (bConnect) {
-					string = dataInputStream.readUTF();
-					System.out.println(string);
-				}
-				// dataInputStream.close();
-
+				Client client = new Client(socket);
+				new Thread(client).start();
 			}
-
-		} catch (EOFException e) {
-			System.out.println("client close");
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		}
+
+	}
+
+	class Client implements Runnable {
+		private Socket socket;
+		private DataInputStream dataInputStream = null;
+		private boolean bConnect = false;
+
+		public Client(Socket socket) {
+			this.socket = socket;
 			try {
-				if (dataInputStream != null) {
-					dataInputStream.close();
-				}
-				if (socket != null) {
-					socket.close();
-				}
+				bConnect = true;
+				dataInputStream = new DataInputStream(socket.getInputStream());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 
-	}
+		@Override
+		public void run() {
+			try {
+				while (bConnect) {
+					String string = dataInputStream.readUTF();
+					System.out.println(string);
+				}
+				// dataInputStream.close();y
 
+			} catch (EOFException e) {
+				System.out.println("client close");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (dataInputStream != null) {
+						dataInputStream.close();
+					}
+					if (socket != null) {
+						socket.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
